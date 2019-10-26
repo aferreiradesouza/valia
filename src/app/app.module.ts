eace7742e08d2aa41818a9121fd277f0d57daefe
Arthur Ferreira <arthur.ferreira@ed.company>
2019-10-25 22:16:04 -0300

    aplicacao da leitura do csv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './paginas/home/home.component';
 import { LoginComponent } from './paginas/login/login.component';
 import { AppRouting } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmpregadosEnum } from './enums/empregados.enum';
+import { ContribuicoesEnum } from './enums/contribuicoes.enum';
+import { DependentesEnum } from './enums/dependentes.enum';
+import { In26Enum } from './enums/in26.enum';
+import { StorageMemoryService } from './services/storage-memory.service';
 
 const PAGINAS = [
   HomeComponent,
   LoginComponent
+];
+
+const ENUMS = [
+  EmpregadosEnum,
+  ContribuicoesEnum,
+  DependentesEnum,
+  In26Enum
 ]
 
 @NgModule({
@@ -20,9 +33,10 @@ const PAGINAS = [
   imports: [
     AppRouting,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [...ENUMS, StorageMemoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild } from '@angular/core';
+import { EmpregadosEnum } from 'src/app/enums/empregados.enum';
+import { StorageMemoryService } from 'src/app/services/storage-memory.service';
 
 @Component({
     selector: 'app-home',
@@ -6,7 +8,76 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-    constructor() { }
+    public csvContent: string;
+    public headers: any[] = [];
+    public layout_patrocinador = 0;
+    public layout_valia = 0;
+    public listaLayoutValia = EmpregadosEnum.obterEnum().sort((a, b) => a.id - b.id);
+    public lista: Array<{layout_patrocinador: any, layout_valia: number}> = [];
 
-    ngOnInit() { }
+    @ViewChild('input', {static: false}) input: any;
+
+    constructor(public storageService: StorageMemoryService) { }
+
+    ngOnInit() {
+    }
+
+    formatarLabel(codigo: number) {
+        return EmpregadosEnum.obterDescricao(codigo);
+    }
+
+    onFileSelect(input: HTMLInputElement) {
+        const files = input.files;
+        if (files && files.length) {
+
+            const fileToRead = files[0];
+            const fileReader = new FileReader();
+            fileReader.onload = (fileLoadedEvent: any) => {
+                const textFromFileLoaded = fileLoadedEvent.target.result;
+                const csvContent = <any>textFromFileLoaded;
+                const result = csvContent.split(/(\r\n|\n|\r)/gm)[0].split(';');
+                this.headers = result.sort();
+            };
+            fileReader.readAsText(fileToRead, "UTF-8");
+        }
+    }
+
+    relacionar() {
+        if(typeof this.layout_valia === 'string') {
+            this.layout_valia = parseInt(this.layout_valia);
+        }
+        const data = {layout_patrocinador: this.layout_patrocinador, layout_valia: this.layout_valia};
+        this.lista.unshift(data);
+        this.listaLayoutValia = this.listaLayoutValia.filter(e => e.id !== this.layout_valia);
+        this.headers = this.headers.filter(e => e !== this.layout_patrocinador);
+        this.layout_valia = 0;
+        this.layout_patrocinador = 0;
+    }
+
+    formatarLabelLista(num: number) {
+        return EmpregadosEnum.obterDescricao(num)
+    }
+
+    remover(item) {
+        console.log(item);
+        const dataLayoutValia = {id: item.layout_valia, name: EmpregadosEnum.obterEnum().filter(e => e.id === item.layout_valia)[0].name};
+        this.listaLayoutValia.push(dataLayoutValia);
+        this.headers.push(item.layout_patrocinador);
+        this.headers.sort();
+        this.listaLayoutValia.sort((a, b) => a.id - b.id);
+        this.lista = this.lista.filter(e => e.layout_valia !== item.layout_valia);
+    }
+
+    salvar() {
+        this.reset();
+    }
+
+    reset() {
+        this.layout_valia = 0;
+        this.layout_patrocinador = 0;;
+        this.input.nativeElement.value = '';
+        this.headers = [];
+        console.log(this.lista);
+        this.lista = [];
+    }
 }
